cb93f500b9455d821166595d6d138503b580c4cf
Mr-Zee <911mrzee@gmail.com>
2022-11-22 21:35:52 +0530

    layout contents

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <div className="nav">Nav</div>
+      <div className="hero">Hero</div>
+      <div className="about">about</div>
+      <div className="cards">cards</div>
+      <div className="form">SignUp-form</div>
+      <div className="footer">footer</div>
     </div>
   );
 }
