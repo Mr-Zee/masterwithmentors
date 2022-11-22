d400b65f21a8af63ca000736529a6ee123ddb444
Mr-Zee <911mrzee@gmail.com>
2022-11-22 22:30:15 +0530

    navbar alignment and responsiveness

---
+++ src/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar section">
+      <h2>Mentors</h2>
+      <div className="nav-items">
+        <a className="nav-item" href="">
+          sample
+        </a>
+        <a className="nav-item" href="">
+          sample
+        </a>
+        <a className="nav-item" href="">
+          sample
+        </a>
+        <a className="nav-item" href="">
+          sample
+        </a>
+      </div>
+      <button className="reg-btn">Register</button>
+    </div>
+  );
+}
+
+export default Navbar;
