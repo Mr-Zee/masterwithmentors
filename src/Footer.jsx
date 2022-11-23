f2696d31e2c95f375c6640443fab5c4052cbac23
Mr-Zee <911mrzee@gmail.com>
2022-11-23 10:39:49 +0530

    footer layout

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,16 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer section">
-      <p>Footer</p>
+      <div className="map">
+        <h1>MAP</h1>
+      </div>
+      <div className="contact">
+        <p>
+          1st Cross Road <br />
+          new York 11 <br />
+          +124 111 765 568 <br />
+        </p>
+      </div>
     </div>
   );
 }
