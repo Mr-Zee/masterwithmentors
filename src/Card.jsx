19165f09f9c80e4a6d25203ead3b5c1269be08b3
Mr-Zee <911mrzee@gmail.com>
2022-11-24 18:55:24 +0530

    image updated

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,8 +4,10 @@ function Card() {
   return (
     <div className="card">
       <img src="./image/img.webp" alt="" />
-      <h3 className="card-title">Title</h3>
-      <p className="card-desc">title desc</p>
+      <h3 className="card-title">Coach</h3>
+      <p className="card-desc">
+        You can choose life of coach that best suits your needs
+      </p>
     </div>
   );
 }
