04a59a6c8b05583bd879efcba65ec5140323f2c6
Mr-Zee <911mrzee@gmail.com>
2022-11-25 12:36:29 +0530

    testimonial props

--- src/T-card.jsx
+++ src/T-card.jsx
@@ -1,30 +1,14 @@
 import React from "react";
 
-function TCard() {
+function TCard(props) {
   return (
-    <>
-      <div className="tcard">
-        <p className="tcard-desc">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit sunt
-          maiores, quaerat eligendi quisquam labore officiis corporis!
-        </p>
-        <div className="tdown">
-          <img src="./image/men.png" alt="" />
-          <h5 className="tcard-title">Bob</h5>
-        </div>
+    <div className="tcard">
+      <p className="tcard-desc">{props.title}</p>
+      <div className="tdown">
+        <img src={props.img} alt="" />
+        <h5 className="tcard-title">{props.name}</h5>
       </div>
-      <div className="tcard">
-        <p className="tcard-desc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-          aut quis omnis officia excepturi eius ratione laudantium quia amet
-          commodi.
-        </p>
-        <div className="tdown">
-          <img src="./image/lady.png" alt="" />
-          <h5 className="tcard-title">Julie</h5>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
 
--- src/Testimonial.jsx
+++ src/Testimonial.jsx
@@ -2,13 +2,39 @@ import React from "react";
 import TCard from "./T-card";
 
 function Testimonial() {
+  const items = [
+    {
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibusaut quis omnis officia excepturi eius ratione laudantium quia ametcommodi.   ",
+      name: "Bob",
+      img: "./image/men.png",
+    },
+    {
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibusaut quis omnis officia excepturi eius ratione laudantium quia ameth",
+      name: "Julie",
+      img: "./image/lady.png",
+    },
+    {
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibusaut quis omnis officia excepturi eius ratione laudantium quia amet ",
+      name: "jack",
+      img: "./image/men.png",
+    },
+    {
+      title:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibusaut quis omnis officia excepturi eius ratione laudantium quia amet",
+      name: "jean",
+      img: "./image/lady.png",
+    },
+  ];
   return (
     <div className="tcards section">
-      <h1>Let's Hear From Our Learners</h1>
       <div className="tlists">
-        <TCard />
-        <TCard />
-        
+        {items.map((item) => (
+          <TCard title={item.title} name={item.name} img={item.img} />
+        ))}
+        <h1>Let's Hear From Our Learners</h1>
       </div>
     </div>
   );
