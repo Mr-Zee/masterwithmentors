645eb05fc47f4b70481e27b683a24b55453f10cd
Mr-Zee <911mrzee@gmail.com>
2022-11-25 18:50:11 +0530

    footer map image

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function About() {
+  const paraRef = useRef(null);
+  const executeScroll = () => {
+    console.log("clicked");
+    paraRef.current.scrollIntoView();
+  };
   return (
     <div className="about section">
-      <h2>About</h2>
-      <p>
+      <h2 onClick={executeScroll}>About</h2>
+      <p ref={paraRef}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, dolore
         earum blanditiis voluptas eveniet obcaecati id laboriosam totam atque
         neque, adipisci excepturi, error voluptates suscipit ratione facilis
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,14 +5,17 @@ function Footer() {
     <>
       <div className="footer section">
         <div className="map">
-          <h1>MENTORS</h1>
+          <a href="https://www.google.com/maps/place/Hanoi,+Ho%C3%A0n+Ki%E1%BA%BFm,+Hanoi,+Vietnam/@21.022736,105.801944,13z/data=!3m1!4b1!4m5!3m4!1s0x3135ab9bd9861ca1:0xe7887f7b72ca17a9!8m2!3d21.0277644!4d105.8341598">
+            <img src="./image/map.webp" alt="" />
+          </a>
         </div>
         <div className="contact">
           <p>
             12nd Floor <br />
             ABC building <br />
             2nd street <br />
-            Washington US <br />
+            Hanoi <br />
+            Vietnam <br />
             +171 765 876 654 <br />
             mentor@mail.com
           </p>
