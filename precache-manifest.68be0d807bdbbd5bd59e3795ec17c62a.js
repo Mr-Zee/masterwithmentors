4dbe4f2dd0eb20d6f11c543759053bf68e0c2208
Mr-Zee <911mrzee@gmail.com>
2022-11-26 01:26:10 +0530

    build

---
+++ precache-manifest.68be0d807bdbbd5bd59e3795ec17c62a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ea967ed5aff8bafe726",
+    "url": "/static/css/main.375e0162.chunk.css"
+  },
+  {
+    "revision": "9ea967ed5aff8bafe726",
+    "url": "/static/js/main.9ea967ed.chunk.js"
+  },
+  {
+    "revision": "02e73066ff27f2a42269",
+    "url": "/static/js/1.02e73066.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf13e4f143ccb2af933b7f7ec5d190f9",
+    "url": "/index.html"
+  }
+];
