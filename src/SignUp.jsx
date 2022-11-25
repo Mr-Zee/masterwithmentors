06f9272d876e3a5e79c971707ac3c0d6da4dd954
Mr-Zee <911mrzee@gmail.com>
2022-11-26 01:17:27 +0530

    Ui fix

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,6 +1,12 @@
 import React, { forwardRef } from "react";
 
 const SignUp = forwardRef((props, ref) => {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [contact, setContact] = React.useState("");
+  const [experience, setExperience] = React.useState(0);
+  const [isMentor, setIsMentor] = React.useState(false);
+
   function saveUser(e) {
     e.preventDefault();
   }
@@ -27,14 +33,33 @@ const SignUp = forwardRef((props, ref) => {
         />
         <div className="radio">
           <label>
-            <input type="radio" value="Mentor" name="mentor" />
+            <input
+              type="radio"
+              value="Mentor"
+              name="mentor"
+              onClick={(e) => {
+                setIsMentor(true);
+              }}
+              checked={isMentor}
+            />
             <span>I am a Mentor</span>
           </label>
           <label>
-            <input type="radio" value="User" name="user" />
+            <input
+              type="radio"
+              value="User"
+              name="user"
+              onClick={(e) => {
+                setIsMentor(false);
+              }}
+              checked={!isMentor}
+            />
             <span> I Need a Mentor</span>
           </label>
         </div>
+        {isMentor ? (
+          <input type="number" placeholder="Experience" className="input" />
+        ) : null}
         <input
           type="submit"
           className="form-btn"
